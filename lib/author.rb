8812409fee37bc6cbe50349e24f29fb61a73d2fc
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-19 12:22:20 -0500

    finished ruby objects belong to lab

---
+++ lib/author.rb
@@ -0,0 +1,7 @@
+class Author
+    attr_accessor :name
+
+    def initialize(name = "Uncle Bob")
+        @name = name
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+class Song
+    attr_accessor :artist, :title
+
+    def initialize(title = "7/11")
+        @title = title
+    end
+end
